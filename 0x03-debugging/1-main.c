4808d0d108a65e7ffd7c16fe861842aea8f20f2a
Femzysamie <femzysamiejebty@gmail.com>
2023-02-21 04:48:31 -0800

    C debbuging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+lines (19 sloc)  247 Bytes
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	/**
+	* i = 0;
+	*
+	* while (i < 10)
+	* {
+	*	putchar(i);
+	* }
+	*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
